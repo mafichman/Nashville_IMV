41ee4d5626b346083d03f5190bf150fc6d275da2
Michael Fichman <mfichman@gmail.com>
2023-02-28 17:04:20 -0500

    Update metro_data_exploration.R

--- R/metro_data_exploration.R
+++ R/metro_data_exploration.R
@@ -2,12 +2,31 @@
 
 library(tidyverse)
 library(sf)
+library(foreign)
 
-parcels <- read_sf("~/GitHub/Nashville_IMV/data/metro/Nashville_Parcel_data.shp")
+# The CRS we are using is: 2274 - Tennessee State Plane (that's what Metro's data came in)
+
+# Older routine - take the data Metro sent (ESRI formats) and save it in faster data forms like geojson
+# Sofia you can use this to adjust the data in your gitignore folder
+
+#parcels <- read_sf("~/GitHub/Nashville_IMV/data/metro/Nashville_Parcel_data.shp")
+
+#parcels %>%
+#  st_transform(4326) %>%
+#st_write("~/GitHub/Nashville_IMV/data/metro/Nashville_Parcel_data.geojson")
+
+#licenses <- read.dbf("~/GitHub/Nashville_IMV/data/metro/Business_Property.dbf")
+
+#licenses %>% write.csv("~/GitHub/Nashville_IMV/data/metro/Business_Property.csv")
 
 permits <- read_sf("~/GitHub/Nashville_IMV/data/metro/Permits_from2010.shp")
 
 Prop_LIS <- read_sf("~/GitHub/Nashville_IMV/data/metro/Prop_LIS_Jan_2023/Prop_LIS_Jan_2023.shp")
 
+# these are some newer, speedier data sets I wrote out
 
-# licenses <- read.table("~/GitHub/Nashville_IMV/data/metro/Business_Property.dbf")
+parcels <- read_sf("~/GitHub/Nashville_IMV/data/metro/Nashville_Parcel_data.geojson") %>%
+  st_transform(2274)
+
+licenses <- read.csv("~/GitHub/Nashville_IMV/data/metro/Business_Property.csv")
+
